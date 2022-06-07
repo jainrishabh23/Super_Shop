3ebe68847a00d34fc03ebb11f4f70bdeca4511cd
Rishabh Jain <106752253+jainrishabh23@users.noreply.github.com>
2022-06-07 19:01:49 +0530

    Project in Progress

---
+++ 1.js
@@ -0,0 +1,44 @@
+var MainImg = document.getElementById("MainImg");
+var smallimg = document.getElementsByClassName("small-img");
+smallimg[0].onclick = function () {
+    MainImg.src = smallimg[0].src;
+}
+smallimg[1].onclick = function () {
+    MainImg.src = smallimg[1].src;
+}
+smallimg[2].onclick = function () {
+
+    MainImg.src = smallimg[2].src;
+}
+smallimg[3].onclick = function () {
+    MainImg.src = smallimg[3].src;
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("demo");
+    let captionText = document.getElementById("caption");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    captionText.innerHTML = dots[slideIndex - 1].alt;
+}
